fe82215a2e835f2c62b1579cdeb96e5ba0cd7fd8
Alexandre <alexandreaddres@gmail.com>
2024-02-09 18:12:21 -0300

    create poll route structure created with relation active

--- src/resources/polls-resource.ts
+++ src/resources/polls-resource.ts
@@ -6,17 +6,25 @@ import {z} from "zod"
 export async function createPoll(app: FastifyInstance){
     app.post('/polls', async (req, reply) => {
         const createPollBody = z.object({
-            title: z.string()
+            title: z.string(),
+            options: z.array(z.string()),
         })
 
-        const {title} = createPollBody.parse(req.body)
+        const {title, options} = createPollBody.parse(req.body)
 
-        const poll = await prisma.poll.create({
+        const poll = await prisma.polls.create({
             data: {
                 title,
+                options: {
+                    createMany: {
+                        data: options.map(option => {
+                            return {title: option}
+                        }),
+                    }
+                },
             }
         })
-    
+
         return reply.status(201).send({poll_key: poll.poll_key})
     
     })
